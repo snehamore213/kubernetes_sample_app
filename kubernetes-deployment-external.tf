6d0a94ef75c59da9a543ef05f762a913153018ad
snehamore213 <36546461+snehamore213@users.noreply.github.com>
2022-10-12 14:30:49 -0400

    Update kubernetes-deployment-external.tf

--- kubernetes-deployment-external.tf
+++ kubernetes-deployment-external.tf
@@ -9,7 +9,7 @@ resource "kubernetes_deployment" "external-app-deployment" {
 
   spec {
     replicas                  = 2
-    progress_deadline_seconds = 60
+    progress_deadline_seconds = 120
     selector {
       match_labels = {
         App = "external-app-deployment"
