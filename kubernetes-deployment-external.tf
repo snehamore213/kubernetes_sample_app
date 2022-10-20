134107664328a5e59a6cb1cc4ce2e4d9ef779b55
Your Name <sneha08820@gmail.com>
2022-10-20 14:38:54 +0000

    some changes

--- kubernetes-deployment-external.tf
+++ kubernetes-deployment-external.tf
@@ -28,10 +28,10 @@ resource "kubernetes_deployment" "external-app-deployment" {
           env {
             name = "SERVER"
             #             value: 'http://34.122.171.52'
-            value = 'http://internal-lb-svc'
+            value = "http://internal-lb-svc"
           }
           port {
-            container_port = '8080'
+            container_port = "8080"
           }
 
           resources {
--- terraform.tfvars
+++ terraform.tfvars
@@ -4,8 +4,5 @@ gcp_region_1 = "us-central1"
 gcp_zone_1   = "us-central1-c"
 
 # Application Name (used in resource names, no spaces.)
-<<<<<<< HEAD
-app_name = "devops-capstone"
-=======
 app_name = "devops-practice"
->>>>>>> f5e515c4147d904ef674fe40bf7ff46342d44d49
+
